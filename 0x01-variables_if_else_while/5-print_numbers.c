0e4b863b71c824097dfbe726667cba049dce3f5e
chuma <chibueze2805@gmail.com>
2022-04-10 04:09:37 -0700

    single digit from 0-10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int single_digit;
+for (single_digit = 0; single_digit <= 10; single_digit++)
+{
+printf("%d\n", single_digit);
+}
+return (0);
+}
