e7e15e5a9f1c7fd62578b80aa0104a6ce9263875
chuma <chibueze2805@gmail.com>
2022-04-10 03:54:40 -0700

    lower case with exceptions

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+char alphabet_lower;
+for (alphabet_lower = 'a'; alphabet_lower <= 'z'; alphabet_lower++)
+{
+if (alphabet_lower == 'q'|| alphabet_lower == 'e')
+{
+continue;
+}
+else
+{
+putchar(alphabet_lower);
+}
+putchar('\n');
+return (0);
+}
