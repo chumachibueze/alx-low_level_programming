3ad6f5b14a941f8d9739ff306c0ff4ffc5db023e
Chuma Chibueze <102015137+chumachibueze@users.noreply.github.com>
2022-04-14 04:26:10 +0100

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,7 +8,7 @@
 void print_times_table(int n)
 {
 int num, mult, prod;
-if (n >= 0 && n < 15)
+if (n >= 0 && n <= 15)
 {
 for (num = 0; num <= n; num++)
 {
@@ -29,12 +29,12 @@ if (prod <= 9)
 _putchar(' ');
 }
 
-if (prod > 100)
+if (prod >= 100)
 {
 _putchar((prod / 100) + '0');
 _putchar(((prod / 100)) % 10 + '0');
 }
-else if (prod <= 99 && prod > 10)
+else if (prod <= 99 && prod >= 10)
 {
 _putchar((prod / 10) + '0');
 }
