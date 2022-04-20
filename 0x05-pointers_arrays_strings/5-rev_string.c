528ff7236f17255826045fd18c74093be6384d52
Chuma Chibueze <102015137+chumachibueze@users.noreply.github.com>
2022-04-21 00:16:36 +0100

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,15 +2,16 @@
 /**
  * rev_string - prints a reverse string
  *@s: A pointer to an int that will be changed
+ *
  *Return: void
  */
 
 void rev_string(char *s)
 {
-
+char *start_c, *end_c, c;
 int i, count;
 int length = 0;
-char *start_c, *end_c;
+
 for (i = 0; s[i]; i++)
 {
 length++;
@@ -25,5 +26,19 @@ end_c = s;
 for (i = 0; i < count - 1; i++)
 {
 end_c++;
+}
+
+
+for (i = 0; i < count / 2; i++)
+{
+
+
+c = *end_c;
+*end_c = *start_c;
+*start_c = c;
+
+
+start_c++;
+end_c--;
 }
 }
