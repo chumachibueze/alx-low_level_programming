7a7618ec5748297c371905ccf277f13a6b183218
Chuma Chibueze <102015137+chumachibueze@users.noreply.github.com>
2022-05-05 03:48:51 +0100

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,9 +1,8 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
-#include <main.h>
-
 /**
  * check_num - check - string there are digit
  * @str: array str
