4fab895015dac099abe7f1dc997f085d3c47b51f
Chuma Chibueze <102015137+chumachibueze@users.noreply.github.com>
2022-05-17 04:58:48 +0100

    Update 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -5,7 +5,6 @@ s (20 sloc)  430 Bytes
 /**
  * sum_them_all - returns the sum of all its parameters.
  * @n: amount of the arguments.
- *
  * Return: sum of its parameters.
  */
 int sum_them_all(const unsigned int n, ...)
